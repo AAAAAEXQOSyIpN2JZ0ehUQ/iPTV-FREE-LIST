b2c79e2a161c665167b92843a3c57dec870120be
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2023-09-14 03:00:20 -0300

    Update Script

--- Install/gestorextream-ui/gestorextream-ui.sh
+++ Install/gestorextream-ui/gestorextream-ui.sh
@@ -6,10 +6,10 @@ x="ok"
 # apt-get upgrade -y 
 # apt-get autoremove -y 
 # apt-get autoclean -y 
-# apt-get iperf3 wget git curl nano bzip2 gzip zip unzip dos2unix iftop htop nload speedometer screen perl bc ca-certificate 
+# apt-get install iperf3 wget git curl nano bzip2 gzip zip unzip dos2unix iftop htop nload speedometer screen perl bc ca-certificate -y
 # apt-get dpkg-reconfigure tzdata
-# apt-get install software-properties-common build-essential 
-# apt-get apt-get install libxslt1-dev libcurl3 libgeoip-dev python -y 
+# apt-get install software-properties-common build-essential -y
+# apt-get install libxslt1-dev libcurl3 libgeoip-dev python -y
 # 
 # rm -rf install.py* 
 # wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/install.py 
@@ -24,59 +24,64 @@ x="ok"
 # /home/xtreamcodes/iptv_xtream_codes/start_services.sh 
 #-----------------------------------------------------------------------------------
 
+if [ "$EUID" -ne 0 ]; then
+  echo -e "\n\033[1;31mPor favor, ejecuta este script como root o usando sudo\n\033[0m"
+  exit
+fi
+
 fun_gestorinstall ()
 {
 install_libraries () {
-echo -e "\n\033[1;36mAGUARDE...\n\033[0m"
+echo -e "\n\033[1;36mAGUARDE...\n\033[1;32m"
 apt-get update -y 
 apt-get upgrade -y 
 apt-get autoremove -y 
 apt-get autoclean -y 
-apt-get iperf3 wget git curl nano bzip2 gzip zip unzip dos2unix iftop htop nload speedometer screen perl bc ca-certificate 
+apt-get install iperf3 wget git curl nano bzip2 gzip zip unzip dos2unix iftop htop nload speedometer screen perl bc ca-certificate -y
 apt-get dpkg-reconfigure tzdata 
-apt-get install software-properties-common build-essential 
-apt-get apt-get install libxslt1-dev libcurl3 libgeoip-dev python -y
+apt-get install software-properties-common build-essential -y
+apt-get install libxslt1-dev libcurl3 libgeoip-dev python -y
 }
 
 install_xtreamui () {
-echo -e "\n\033[1;36mAGUARDE...\n\033[0m"
+echo -e "\n\033[1;36mAGUARDE...\n\033[1;32m"
 rm -rf $HOME/install.py* && wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/install.py && python install.py
 }
 
 permissions_nginx () {
-echo -e "\n\033[1;36mAGUARDE...\n\033[0m"
+echo -e "\n\033[1;36mAGUARDE...\n\033[1;32m"
 chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp
 chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/nginx 
 /home/xtreamcodes/iptv_xtream_codes/start_services.sh
 }
 
 tables_file () {
-echo -e "\n\033[1;36mAGUARDE...\n\033[0m"
+echo -e "\n\033[1;36mAGUARDE...\n\033[1;32m"
 touch /home/xtreamcodes/iptv_xtream_codes/admin/.update 
 /home/xtreamcodes/iptv_xtream_codes/start_services.sh 
 /home/xtreamcodes/iptv_xtream_codes/start_services.sh
 }
 
 backups_file () {
-echo -e "\n\033[1;36mAGUARDE...\n\033[0m"
+echo -e "\n\033[1;36mAGUARDE...\n\033[1;32m"
 rm -rf $HOME/download.sh* && bash <(wget -qO- https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/iPTV-FREE-LIST/master/Install/gestorextream-ui/download.sh)
 }
 
 while true $x != "ok"
 do
 clear
-echo -e "\033[1;32m——————————————————————————————————————————————————————\033[0m"
-echo -e "\033[1;37m               GERENCIAR XTREAM-UI R22F            \033[0m"
-echo -e "\033[1;32m——————————————————————————————————————————————————————\033[0m"
+echo -e "\033[1;32m===============================================================\033[0m"
+echo -e "\033[1;37m               GERENCIAR XTREAM-UI R22F\033[0m"
+echo -e "\033[1;32m===============================================================\033[0m"
+echo -e "\033[1;31mID\033[1;32m: \033[1;37m$USER\033[0m"
+echo -e "\033[1;32m===============================================================\033[0m"
 echo ""
-echo -e "\033[1;31mID\033[1;32m: \033[1;37m$USER"
-echo ""
-echo -e "\033[1;31m[\033[1;36m01\033[1;31m] \033[1;37m• \033[1;33mINSTALL LIBRARIES \033[1;31m   
-[\033[1;36m02\033[1;31m] \033[1;37m• \033[1;33mINSTALL XTREAM-UI R22F \033[1;31m                  
-[\033[1;36m03\033[1;31m] \033[1;37m• \033[1;33mNGINX PERMISSIONS \033[1;31m
-[\033[1;36m04\033[1;31m] \033[1;37m• \033[1;33mTABLES AND .UPDATE FILE \033[1;31m
-[\033[1;36m05\033[1;31m] \033[1;37m• \033[1;33mBACKUPS LIBRARIES AND FILES \033[1;31m
-[\033[1;36m00\033[1;31m] \033[1;37m• \033[1;33mEXIT THE MENU \033[1;32m<\033[1;33m<\033[1;31m< \033[1;31m"
+echo -e "\033[1;31m[\033[1;32m01\033[1;31m] \033[1;37m> \033[1;32mINSTALL LIBRARIES\033[0m"
+echo -e "\033[1;31m[\033[1;32m02\033[1;31m] \033[1;37m> \033[1;32mINSTALL XTREAM-UI R22F\033[0m"
+echo -e "\033[1;31m[\033[1;32m03\033[1;31m] \033[1;37m> \033[1;32mNGINX PERMISSIONS\033[0m"
+echo -e "\033[1;31m[\033[1;32m04\033[1;31m] \033[1;37m> \033[1;32mTABLES AND .UPDATE FILE\033[0m"
+echo -e "\033[1;31m[\033[1;32m05\033[1;31m] \033[1;37m> \033[1;32mBACKUPS LIBRARIES AND FILES\033[0m"
+echo -e "\033[1;31m[\033[1;32m00\033[1;31m] \033[1;37m> \033[1;37mEXIT \033[1;32m<\033[1;33m<\033[1;31m< \033[1;31m\033[0m"
 echo ""
 echo ""
 echo -e "\033[1;32m┌─[\033[1;31mwhat do you want to do\033[1;32m]──[\033[1;37m~\033[1;32m]─[\033[1;37m$(pwd)\033[1;32m]"
