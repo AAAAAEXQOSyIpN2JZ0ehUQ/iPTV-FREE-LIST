26d59372de8b82c1fea13c10ad9424d6e4c897ce
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2023-09-14 03:05:21 -0300

    Update Script

--- Install/gestorextream-ui/gestorextream-ui.sh
+++ Install/gestorextream-ui/gestorextream-ui.sh
@@ -64,7 +64,32 @@ touch /home/xtreamcodes/iptv_xtream_codes/admin/.update
 
 backups_file () {
 echo -e "\n\033[1;36mAGUARDE...\n\033[1;32m"
-rm -rf $HOME/download.sh* && bash <(wget -qO- https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/iPTV-FREE-LIST/master/Install/gestorextream-ui/download.sh)
+cd
+mkdir xtreamui_mirror
+cd xtreamui_mirror
+wget https://bitbucket.org/emre1393/xtreamui_mirror/raw/a86a37504b2f619201467fdd121a9786d7da1793/README.md
+wget https://bitbucket.org/emre1393/xtreamui_mirror/raw/a86a37504b2f619201467fdd121a9786d7da1793/install.py
+mkdir Download_repository
+cd Download_repository
+wget https://bitbucket.org/emre1393/xtreamui_mirror/get/a86a37504b2f.zip
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/Geolite2_status.json
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/GeoLite2.mmdb
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/nginx_rtmp
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/nginx
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/balancer.py
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/install.py
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/newstuff.zip
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/xmltv.php
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/player_api.php
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/panel_api.php
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/release_22f.zip
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/xc_sorted_devices.sql
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/config.py
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/clearlogs.sh
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/pid_monitor.php
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/sub_xtreamcodes_reborn.tar.gz
+wget https://bitbucket.org/emre1393/xtreamui_mirror/downloads/main_xtreamcodes_reborn.tar.gz
+cd
 }
 
 while true $x != "ok"
@@ -115,7 +140,8 @@ case "$x" in
    ;;
    update)
    clear
-   wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/iPTV-FREE-LIST/master/Install/gestorextream-ui/gestorextream-ui.sh && chmod +x gestorextream-ui.sh* && ./gestorextream-ui.sh*
+   echo -e "\n\033[1;36mAGUARDE...\n\033[1;32m"
+   wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/iPTV-FREE-LIST/master/Install/gestorextream-ui/gestorextream-ui.sh && chmod +x gestorextream-ui.sh*
    echo -ne "\n\033[1;31mENTER \033[1;33mto return to \033[1;32mMENU!\033[0m"; read
    ;;
    0 | 00)
